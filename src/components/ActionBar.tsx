2f3a783495629a19633629cb365df197eaf1167d
DP-Mani <mani.n@datapoem.com>
2025-01-19 00:59:33 +0530

    fix: type error fixed for unused var

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Undo2, Redo2 } from 'lucide-react'
-import { useOthers } from '@liveblocks/react/suspense'
 
 interface ActionBarProps {
 	onUndo: () => void
@@ -8,7 +7,6 @@ interface ActionBarProps {
 }
 
 const ActionBar = ({ onUndo, onRedo }: ActionBarProps) => {
-	const others = useOthers()
 	return (
 		<div className="p-4 absolute bottom-0 left-4 flex gap-3">
 			<button
